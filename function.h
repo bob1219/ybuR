2edd88f2ab840e8641961b9550e4d4473a1f73a2
吉田大輝 <bob181219@yahoo.co.jp>
2018-05-04 13:20:39 +0900

    Describe declaration of ybur::interpret function

---
+++ function.h
@@ -0,0 +1,30 @@
+// Copyright 2018 Daiki Yoshida. All rights reserved.
+//
+// This file is part of ybuR.
+//
+// ybuR is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ybuR is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ybuR. If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+// standard library
+#include <vector>
+#include <string>
+
+namespace ybur
+{
+	void interpret(const std::vector<std::wstring>& lines);
+}
+
+#endif
